3b82f7144cbea28ce5ee2ad7b851c38a84585685
Qian Tang <q.tang6@student.unimelb.edu.au>
2020-12-19 21:52:35 +1100

    use computed properties in the composition API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
+    <input type="text" v-model="search">
+    <p>Search term : {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">{{name}}</div>
+    <button @click="handleClick">Stop Watching</button>
+    
+
+
+    <!-- <h2>Refs</h2>
     <p>{{ninjaOne.name}} - {{ninjaOne.age}}</p>
     <button @click="updateNinjaOne">Update ninja one</button>
     <h2>Reactive</h2>
     <p>{{ninjaTwo.name}} - {{ninjaTwo.age}}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <button @click="updateNinjaTwo">Update ninja two</button> -->
     
     <!-- Home
     <p >My name is {{name}} and my age is {{age}}</p>
@@ -17,21 +24,46 @@
 </template>
 
 <script>
-import { ref,reactive } from 'vue'
+import { ref,reactive, computed, watch, watchEffect } from 'vue'
 
 export default {
   name: 'Home',
   setup(){
-    const ninjaOne = ref({name:'mario',age:30})
-    const ninjaTwo = reactive({name:'luigi',age:35})
-    const updateNinjaOne = ()=>{
-      ninjaOne.value.age =40
-    }
-    const updateNinjaTwo = ()=>{
-      ninjaTwo.age =45
+    const names = ref(['mario','yoshi','luigi','toat','bowser','koopa','peach'])
+    const search = ref('')
+
+    //watch the value we list 
+    const stopWatch = watch(search,()=>{
+      console.log('func run ')
+    })
+
+
+    //watchEffect run initially when the component firstly loaded
+    //when use any value inside it, and when the value changes, it will run 
+    const stopEffect = watchEffect(()=>{
+      console.log('effect func run', search.value)
+    })
+
+    const matchingNames = computed(()=>{
+      return names.value.filter(name => name.includes(search.value))
+    })
+
+    const handleClick = ()=>{
+      stopWatch()
+      stopEffect()
     }
 
-    return {ninjaOne,updateNinjaOne,updateNinjaTwo,ninjaTwo}
+    return {names,search,matchingNames,handleClick}
+    // const ninjaOne = ref({name:'mario',age:30})
+    // const ninjaTwo = reactive({name:'luigi',age:35})
+    // const updateNinjaOne = ()=>{
+    //   ninjaOne.value.age =40
+    // }
+    // const updateNinjaTwo = ()=>{
+    //   ninjaTwo.age =45
+    // }
+
+    // return {ninjaOne,updateNinjaOne,updateNinjaTwo,ninjaTwo}
     // console.log('setup')
     //those two variables are not reactive 
     //which means when it updates, this wont reflect on page
